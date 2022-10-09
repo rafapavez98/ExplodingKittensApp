309317314500e567c65d1fbe7fb7efe5b700c20e
rafapavez98 <repavez@miuandes.cl>
2022-10-09 01:45:35 -0300

    navigation, buttons and views
    Views needed for the second installment created. All navigation working with their respective buttons.

--- app/src/main/java/com/example/explodingkittensapp/HomeScreenFragment.kt
+++ app/src/main/java/com/example/explodingkittensapp/HomeScreenFragment.kt
@@ -22,7 +22,29 @@ class HomeScreenFragment : Fragment() {
 
         logoutbtn.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeScreenFragment_to_loginFragment)}
 
+        val newgamebtn : Button = view.findViewById(R.id.newgamebtn)
+
+        newgamebtn.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeScreenFragment_to_newGameFragment)}
+
+        val joingamebtn : Button = view.findViewById(R.id.joingamebtn)
+
+        joingamebtn.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeScreenFragment_to_joinGameFragment)}
+
+        val mygamesbtn : Button = view.findViewById(R.id.mygamesbtn)
+
+        mygamesbtn.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeScreenFragment_to_myGamesFragment)}
+
+        val profilebtn : Button = view.findViewById(R.id.profilebtn)
+
+        profilebtn.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeScreenFragment_to_profileFragment)}
+
+        val friendsbtn : Button = view.findViewById(R.id.friendsbtn)
+
+        friendsbtn.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_homeScreenFragment_to_friendsFragment)}
+
         return view
+
+
 
     }
 
