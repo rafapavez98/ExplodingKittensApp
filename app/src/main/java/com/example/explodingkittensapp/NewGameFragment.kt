b2a92096e8aada20f011055600ceaaebea0bbfe0
diegoteran99 <diegoteranbaselli@gmail.com>
2022-10-13 18:09:21 -0300

    new game layout implemented

--- app/src/main/java/com/example/explodingkittensapp/NewGameFragment.kt
+++ app/src/main/java/com/example/explodingkittensapp/NewGameFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 
 class NewGameFragment : Fragment() {
@@ -16,7 +18,13 @@ class NewGameFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_new_game, container, false)
 
+        val homebtn : Button = view.findViewById(R.id.homebtn)
 
+        homebtn.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_newGameFragment_to_homeScreenFragment)}
+
+        val friendsbtn : Button = view.findViewById(R.id.friendsbtn)
+
+        friendsbtn.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_newGameFragment_to_friendsFragment)}
 
         return view
     }
